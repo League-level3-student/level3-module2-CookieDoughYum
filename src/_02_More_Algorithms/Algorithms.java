082e664ca5aefa66aaa54d00462668f22f640c21
root <root@204322c23630>
2020-07-20 23:53:52 +0000

    Working on Searching and Sorting Algorithms.

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,22 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for(int i=0; i<eggs.size(); i++) {
+			if(eggs.get(i)=="cracked") {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static Object countPearls(List<Boolean> oysters) {
+		// TODO Auto-generated method stub
+		int pearls=0;
+		for(int i=0; i<oysters.size(); i++) {
+			if(oysters.get(i)==true) {
+				pearls+=1;
+			}
+		}
+		return pearls;
 	}
 }
